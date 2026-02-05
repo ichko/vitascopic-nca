bcfc62c5f6d9f75e9fe38e4595c4537d70920b48
JakobSchauser <jakob.schauser@gmail.com>
2026-02-05 10:36:01 +0100

    Stimuli?

---
+++ src/vitascopic_nca/stimuli.py
@@ -0,0 +1,44 @@
+import torch
+
+
+
+
+
+class Stimuli:
+    """Class to represent stimuli for the NCA. For simplicity, we just use random noise as stimuli."""
+
+    def __init__(self, initial_state):
+        # self.initial_state = initial_state # batch_size, channels, H, W
+        N = 1
+        R = 8
+        # add N circles of radius R to the initial state as stimuli
+        self.stimuli = torch.zeros_like(initial_state[:, :1, :, :])  # only 1 channel for stimuli
+        for _ in range(N):
+            # with peridoic boundary conditions
+            center_x = torch.randint(0, initial_state.shape[3], (1,)).item()
+            center_y = torch.randint(0, initial_state.shape[2], (1,)).item()
+
+            y, x = torch.meshgrid(
+                torch.arange(initial_state.shape[2]), torch.arange(initial_state.shape[3])
+            )
+
+            # with periodic boundary conditions
+            dist_x = torch.min(torch.abs(x - center_x), torch.abs(x - center_x + initial_state.shape[3]))
+            dist_y = torch.min(torch.abs(y - center_y), torch.abs(y - center_y + initial_state.shape[2]))
+            mask = (dist_x) ** 2 + (dist_y) ** 2 <= R**2
+
+
+
+            self.stimuli[:, :, mask] = 1.  # set stimulus value to 0.5
+
+        self.stimuli = self.stimuli.squeeze(1)  # shape (batch_size, H, W)
+        self.stimuli = self.stimuli.to(initial_state.device)
+
+
+    def add_stimuli_noise(self, final_frame):
+        return torch.where(self.stimuli.unsqueeze(1) < 0.5, torch.zeros_like(final_frame), final_frame)
+    
+    def add_stimuli(self, state):
+        state[:, 1, :, :] =  state[:, 1, :, :] + self.stimuli  # add stimuli to the second channel
+
+        return state
